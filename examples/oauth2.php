c8102d13a77e9b8aeb4465b296ff546ea8f70984
Jens Segers <segers.jens@gmail.com>
2018-03-14 14:11:41 +0100

    Add some additional comment blocks to the php example

--- examples/oauth2.php
+++ examples/oauth2.php
@@ -9,15 +9,24 @@ $clientSecret = 'YOUR-CLIENT-SECRET';
 
 /**
  * Where to redirect to after the OAuth 2 flow was completed.
+ * Make sure this matches the information of your integration settings on the marketplace build page.
  */
 $redirectUri = 'http://localhost:3000/oauth.php';
 
-/* --------------------------------------------------------------------------------------------------- */
+/* ------------------------------------------------------------------------------------------------- */
 
+/**
+ * When the OAuth2 authentication flow was completed, the user is redirected back with a code.
+ * If we received the code, we can get an access token and make API calls. Otherwise we redirect
+ * the user to the OAuth2 authorization endpoints.
+ */
 if (!empty($_GET['code'])) {
 
     $code = rawurldecode($_GET['code']);
 
+    /**
+     * Request an access token based on the received authorization code.
+     */
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, 'https://app.teamleader.eu/oauth2/access_token');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -34,6 +43,9 @@ if (!empty($_GET['code'])) {
     $data = json_decode($response, true);
     $accessToken = $data['access_token'];
 
+    /**
+     * Get the user identity information using the access token.
+     */
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, 'https://api.teamleader.eu/users.me');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
